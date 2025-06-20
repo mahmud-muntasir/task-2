b32cb7d96f16631b8c0bfe1b0886c2bd71d0be40
Muntasir Mahmud <muntasirr.mahmudd@gmail.com>
2025-06-20 16:07:02 +0600

    hasbeen fix receiver component issue

--- app/receiver/page.jsx
+++ app/receiver/page.jsx
@@ -1,4 +1,10 @@
-import LocationReceiver from '@/components/LocationReceiver';
+"use client"
+
+import dynamic from 'next/dynamic';
+const LocationReceiver = dynamic(() => import('@/components/LocationReceiver'), {
+  ssr: false,
+});
+// import LocationReceiver from '@/components/LocationReceiver';
 
 export default function ReceiverPage() {
   return <LocationReceiver />;
--- app/sender/page.jsx
+++ app/sender/page.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import LocationSender from '@/components/LocationSender';
 
 export default function SenderPage() {
